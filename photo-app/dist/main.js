321b552b9e58b6c3e418fae675ff60affb71f751
alberto <alberto.floreso94@gmail.com>
2021-08-04 14:40:18 -0500

    add webpack

--- photo-app/dist/main.js
+++ photo-app/dist/main.js
@@ -1,10 +1,17 @@
 "use strict";
+// import { Album, PhotoOrientation, Picture, User } from './photo-app';
 Object.defineProperty(exports, "__esModule", { value: true });
-var photo_app_1 = require("./photo-app");
-var user = new photo_app_1.User(1, 'fribenitez', 'Frida', true);
-var album = new photo_app_1.Album(10, 'Frida Pictures');
-var picture = new photo_app_1.Picture(1, 'Fri', '2020-03', photo_app_1.PhotoOrientation.Lanscape);
+const album_1 = require("./album");
+const photo_orientation_1 = require("./photo-orientation");
+const picture_1 = require("./picture");
+const user_1 = require("./user");
+const user = new user_1.User(1, 'fribenitez', 'Frida', true);
+const album = new album_1.Album(10, 'Frida Pictures');
+const picture = new picture_1.Picture(1, 'Fri', '2020-03', photo_orientation_1.PhotoOrientation.Lanscape);
 // Creamos relaciones
 user.addAlbum(album);
 album.addPicture(picture);
+console.log('user', user);
+// Borramos Album
+user.removeAlbum(album);
 console.log('user', user);
--- photo-app/src/main.ts
+++ photo-app/src/main.ts
@@ -1,4 +1,9 @@
-import { Album, PhotoOrientation, Picture, User } from './photo-app';
+// import { Album, PhotoOrientation, Picture, User } from './photo-app';
+
+import { Album } from "./album";
+import { PhotoOrientation } from "./photo-orientation";
+import { Picture } from "./picture";
+import { User } from "./user";
 
 const user = new User(1, 'fribenitez', 'Frida', true);
 const album = new Album(10, 'Frida Pictures');
@@ -7,4 +12,8 @@ const picture = new Picture(1, 'Fri', '2020-03', PhotoOrientation.Lanscape);
 user.addAlbum(album);
 album.addPicture(picture);
 
+console.log('user', user);
+
+// Borramos Album
+user.removeAlbum(album);
 console.log('user', user);
